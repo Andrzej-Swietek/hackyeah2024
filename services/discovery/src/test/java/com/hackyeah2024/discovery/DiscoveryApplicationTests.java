193d6b725ec32e6ed5839f78eb2e94520ce85922
Andrzej Świętek <mr.andi321@gmail.com>
2024-09-17 20:28:54 +0200

    Added API gateway and eureka

---
+++ services/discovery/src/test/java/com/hackyeah2024/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackyeah2024.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
