2dd0004d95b181dca4dc5fa892b0b45f459bccce
Wiktor Cieślikiewicz <406470@student.agh.edu.pl>
2024-10-01 22:15:24 +0200

    Fixed grpc java client

--- services/feed/src/main/java/com/hackyeah2024/feed/feed/FeedController.java
+++ services/feed/src/main/java/com/hackyeah2024/feed/feed/FeedController.java
@@ -4,10 +4,7 @@ package com.hackyeah2024.feed.feed;
 import com.hackyeah2024.feed.recommender.RecommenderService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/feed")
@@ -19,9 +16,9 @@ public class FeedController {
 
     @GetMapping
     public ResponseEntity<?> getDefaultFeed(
-            @PathVariable(name = "user-id") String userId,
-            @PathVariable(name = "requesterType") String requesterType,
-            @PathVariable(name = "requestedType") String requestedType
+            @RequestParam(name = "user-id") String userId,
+            @RequestParam(name = "requesterType") String requesterType,
+            @RequestParam(name = "requestedType") String requestedType
     ){
         var recommendation = recommenderService.getRecommendation(
                 userId,
