a8b97b3d3f4b36d8bb2cd9f35f3fdc651026468e
Andrzej Świętek <mr.andi321@gmail.com>
2024-10-01 21:22:43 +0200

    Fixed path params

--- services/feed/src/test/java/com/hackyeah2024/feed/FeedApplicationTests.java
+++ services/feed/src/test/java/com/hackyeah2024/feed/FeedApplicationTests.java
@@ -1,13 +1,27 @@
 package com.hackyeah2024.feed;
 
+import com.hackyeah2024.feed.config.RecommenderClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import recommender.RecommenderOuterClass.RequesterType;
+import recommender.RecommenderOuterClass.RequesterType;
+import recommender.RecommenderOuterClass.RequestedType;
 
 @SpringBootTest
 class FeedApplicationTests {
 
 	@Test
 	void contextLoads() {
+		var recommenderClient = new RecommenderClient(
+				"dfdfdfd",
+				RequesterType.REQUESTER_NGO,
+				RequestedType.REQUESTED_VOLUNTEER
+		);
+		recommenderClient.getRecommendations()
+				.forEach(System.out::println);
+
+		assert(!recommenderClient.getRecommendations().isEmpty());
 	}
 
 }
